713444f26858a5fcee05f0833a7664c5058c9f06
ychd_zn1 <zhangning1@belink.com>
2018-09-10 16:01:07 +0800

    cs1

---
+++ test_git_ai/src/main/java/test_git/test_git_ai/App.java
@@ -0,0 +1,13 @@
+package test_git.test_git_ai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
